e9188366898de4e68aac592b311ae001794e73b4
Dani Amsalem <dania@signupgenius.com>
2021-12-22 09:45:59 -0500

    Replacing Apostrophe with HTML Version

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -15,7 +15,7 @@ const AuthorIntro = () =>
             <Media.Body>
                 <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
                 <p className="welcome-text">
-                My name is Dani Amsalem, I'm an experienced web developer
+                My name is Dani Amsalem, I&apos;m an experienced web developer
                 and this is my blog page.
                 </p>
             </Media.Body>
